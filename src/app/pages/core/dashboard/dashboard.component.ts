9aa12b5231dd8e09a29f1407f4cda32a21a57193
ddpt18 <diegopucuji18@gmail.com>
2025-01-17 19:21:49 -0500

    dashboard

--- src/app/pages/core/dashboard/dashboard.component.ts
+++ src/app/pages/core/dashboard/dashboard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  isDropdownOpen: boolean = false; // Variable para controlar el estado del dropdown
 
+  // Alternar el estado del dropdown al hacer clic en el botón
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // Cerrar el dropdown si se hace clic fuera de él
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: Event): void {
+    const dropdownButton = document.getElementById('dropdownUsuarios');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const target = event.target as HTMLElement;
+
+    if (dropdownButton && dropdownMenu) {
+      if (!dropdownButton.contains(target) && !dropdownMenu.contains(target)) {
+        this.isDropdownOpen = false;
+      }
+    }
+  }
 }
