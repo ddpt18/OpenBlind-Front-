9a9cebe80bc8d4c868024fb478434b65876ef1d0
ddpt18 <diegopucuji18@gmail.com>
2025-01-20 11:05:49 -0500

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Servir la carpeta "dist" generada por Angular
+app.use(express.static(path.join(__dirname, 'dist/openblind-front')));
+
+// Redirige todas las rutas al archivo index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/openblind-front/index.html'));
+});
+
+// Usa el puerto asignado por Railway o un puerto local
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
