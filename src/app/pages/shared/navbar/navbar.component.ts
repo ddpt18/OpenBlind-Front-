00fc03c44cce80878e47d40d6982ce1e3002684a
AdrianReyes555 <len555rey@gmail.com>
2025-01-24 12:13:43 -0500

    add

--- src/app/pages/shared/navbar/navbar.component.ts
+++ src/app/pages/shared/navbar/navbar.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  title = 'navbar';
+
+  toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar') as HTMLElement;
+    const mainContent = document.querySelector('.main-content') as HTMLElement;
+    const toggleIcon = document.querySelector('.rotate-icon') as HTMLElement;
+
+    if (sidebar && mainContent && toggleIcon) {
+      sidebar.classList.toggle('collapsed');
+      mainContent.classList.toggle('collapsed');
+      toggleIcon.classList.toggle('rotate');
+    }
+  }
 
 }
